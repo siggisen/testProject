3002df1d603a07ab1a73ecf00ebce391abd7004d
siggisen <34709093+siggisen@users.noreply.github.com>
2018-09-26 10:02:22 +0200

    Added customerSalary
    Added customerSalary

--- src/testTest/Customer.java
+++ src/testTest/Customer.java
@@ -3,6 +3,8 @@ package testTest;
 public class Customer {
 	private String customerID;
 	private String customerName;
+	private int customerSalary;
+	
 	
 	public String getCustomerID() {
 		return customerID;
@@ -16,6 +18,13 @@ public class Customer {
 	public void setCustomerName(String customerName) {
 		this.customerName = customerName;
 	}
+	public int getCustomerSalary() {
+		return customerSalary;
+	}
+	public void setCustomerSalary(int customerSalary) {
+		this.customerSalary = customerSalary;
+	}
+	
 	
 
 }
